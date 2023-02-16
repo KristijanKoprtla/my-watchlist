a9aa2fee5f0cf752768d2d6b4f13ca9178467b00
hyperv <hyperv@ucione.local>
2023-02-16 17:44:15 +0100

    Add route

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,29 @@ import Favorites from "./components/Favorites";
 import Search from "./components/Search";
 import Footer from "./components/Footer";
 
-import Playground from './playground/Playground';
-import {
-  BrowserRouter as Router, Routes, Route
-} from 'react-router-dom'
-
-
-
-
+import Playground from "./playground/Playground";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
     <Router>
-    <div className="App">
-      <Header />
-      
-      <main>
-        <Routes>
-          <Route path="/" element= {<Landing />} />
-          <Route path="/favorites" element= {<Favorites />} />
-        </Routes>
-        <Landing />
-        <Favorites />
-        <Search />
-      </main>
-      
-      <Footer />  
+      <div className="App">
+        <Header />
 
-      
+        <main>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+          <Landing />
+          <Favorites />
+          <Search />
+        </main>
 
-      {/* <Playground /> */}
- 
-    </div>
-    
+        <Footer />
+
+        {/* <Playground /> */}
+      </div>
     </Router>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,18 @@
-import '../styles/navigation.scss';
-import navList from '../data/navigation.json';
+import "../styles/navigation.scss";
+import navList from "../data/navigation.json";
+import { Link } from 'react-router-dom';
 function Navigation() {
-
-
-    return(
-        <nav className='main-nav'>
-            <ul className="navItems">
-                {navList.map((navItem, index) => <li key={index}><a href={navItem.url}>{navItem.title}</a></li>)}
-            </ul>
-            
-
-
-        </nav>
-
-    )
+  return (
+    <nav className="main-nav">
+      <ul className="navItems">
+        {navList.map((navItem, index) => (
+          <li key={index}>
+            <Link to={navItem.url}>{navItem.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
